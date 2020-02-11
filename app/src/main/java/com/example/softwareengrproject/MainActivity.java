c8921affcc347fe447d50dd6acc52e21b4659b11
kelseyziemba <kjz17b@my.fsu.edu>
2020-02-11 18:06:17 -0500

    kelsey's comment

--- app/src/main/java/com/example/softwareengrproject/MainActivity.java
+++ app/src/main/java/com/example/softwareengrproject/MainActivity.java
@@ -25,6 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+        startActivity(intent); //comment
     }
 }
