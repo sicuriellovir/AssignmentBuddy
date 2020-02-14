ea7bfcac080b276441b8c42bcde5d5993c440357
ake1998 <ellis.ashley1998@gmail.com>
2020-02-14 11:40:31 -0500

    Ashley's test comment

--- app/src/main/java/com/example/softwareengrproject/MainActivity.java
+++ app/src/main/java/com/example/softwareengrproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//Ashley's comment
 public class MainActivity extends AppCompatActivity {
     private Button button;
 
