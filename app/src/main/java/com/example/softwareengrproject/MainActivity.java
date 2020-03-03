e6f156c9684325667bc85aded10981a05d631f7f
Analise <analisesaunders@gmail.com>
2020-03-03 02:20:42 -0500

    Squashed commit of the following:
    commit 0683e29e46a3b18f32ec47104cec8956112ef517
    Merge: 7f8c562 d92be12
    Author: Analise <analisesaunders@gmail.com>
    Date:   Tue Mar 3 02:07:35 2020 -0500
    
        Merge branch 'Development' into Home_Screen
    
        # Conflicts:
        #	app/src/main/java/com/example/softwareengrproject/MainActivity.java
        #	app/src/main/res/layout/activity_main.xml
    
    commit 7f8c562ae8ab9f4347d7455359afd07cadb7c81d
    Author: Analise <analisesaunders@gmail.com>
    Date:   Tue Mar 3 01:51:58 2020 -0500
    
        Adjust home screen icon position/layout
    
    commit 1633b57ccaa0459f576a79ded4f972f9b73634e3
    Author: Analise <analisesaunders@gmail.com>
    Date:   Tue Mar 3 01:21:45 2020 -0500
    
        Add interface for home screen - 'login' & 'register' buttons
    
    commit 9b894b9b7402b47610d78f96adee486627cc2cdb
    Author: kelseyziemba <kjz17b@my.fsu.edu>
    Date:   Mon Mar 2 11:26:46 2020 -0500
    
        comment
    
    commit ea7bfcac080b276441b8c42bcde5d5993c440357
    Author: ake1998 <ellis.ashley1998@gmail.com>
    Date:   Fri Feb 14 11:40:31 2020 -0500
    
        Ashley's test comment
    
    commit 9541609e1ab9839173649234d9099945c1c3c378
    Author: virginiasicuriello <ves16c@my.fsu.edu>
    Date:   Thu Feb 13 17:13:54 2020 -0500
    
        Virginia's comment
    
    commit c8921affcc347fe447d50dd6acc52e21b4659b11
    Author: kelseyziemba <kjz17b@my.fsu.edu>
    Date:   Tue Feb 11 18:06:17 2020 -0500
    
        kelsey's comment
    
    commit 34253e4cef5a2ebae5d6671f8229d832c04de5dc
    Author: analisesaunders <analisesaunders@gmail.com>
    Date:   Tue Feb 11 07:46:43 2020 +0000
    
        Merged PR 1: New activity button
    
        **Test 'merge'** : Development branch commits merged **into** master branch - code: when button is clicked, new activity is opened
    
        Related work items: #3

--- app/src/main/java/com/example/softwareengrproject/MainActivity.java
+++ app/src/main/java/com/example/softwareengrproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//Ashley's comment
 public class MainActivity extends AppCompatActivity {
     private Button button;
 
@@ -22,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    //comment Kelsey
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+        startActivity(intent); //comment Virginia
     }
 }
