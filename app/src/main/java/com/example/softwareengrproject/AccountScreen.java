c78f5dd7c0ab4db3e832ffd19db0b22c732b4973
jeffreyknappman <jkk17b>
2020-03-09 17:10:46 -0400

    Added activity_account.xml and AccountScreen.java for the account screen activity. Also added strings to strings.xml.

---
+++ app/src/main/java/com/example/softwareengrproject/AccountScreen.java
@@ -0,0 +1,47 @@
+package com.example.softwareengrproject;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.widget.Button;
+        import android.os.Bundle;
+        import android.util.Log;
+
+public class AccountScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_screen);
+
+        Button buttonCourses = findViewById(R.id.button4);
+        Button buttonTodo = findViewById(R.id.button3);
+        Button buttonMessenger = findViewById(R.id.button5);
+        Button buttonSettings = findViewById(R.id.button6);
+    }
+
+    /*
+    public void openActivityCourse()
+    {
+        Intent intent = new Intent(this, course.class);
+        startActivity(intent);
+    }
+
+    public void openActivityTodo()
+    {
+         Intent intent = new Intent(this, todopage.class);
+         startActivity(intent);
+    }
+
+    public void openActivityMessenger()
+    {
+        Intent intent = new Intent(this, messenger.class);
+        startActivity(intent);
+    }
+
+    public void openActivitySettings()
+    {
+        Intent intent = new Intent(this, settings.class);
+        startActivity(intent);
+    }
+*/
+}
