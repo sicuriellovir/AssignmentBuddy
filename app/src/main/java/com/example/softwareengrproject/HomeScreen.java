545c3d7708e7cf6c344112892e101f48bcb79002
Virginia Sicuriello <ves16c@my.fsu.edu>
2020-03-09 17:57:44 -0400

    Virginia Branch, fixed register screen linking

--- app/src/main/java/com/example/softwareengrproject/HomeScreen.java
+++ app/src/main/java/com/example/softwareengrproject/HomeScreen.java
@@ -1,48 +1,74 @@
 package com.example.softwareengrproject;
-
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.softwareengrproject.LoginScreen;
+import com.example.softwareengrproject.R;
+import com.example.softwareengrproject.RegisterScreen;
+
+import com.example.softwareengrproject.LoginScreen;
+import com.example.softwareengrproject.R;
+import com.example.softwareengrproject.RegisterScreen;
+
+import com.example.softwareengrproject.LoginScreen;
+import com.example.softwareengrproject.R;
+import com.example.softwareengrproject.RegisterScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.softwareengrproject.LoginScreen;
+import com.example.softwareengrproject.R;
+import com.example.softwareengrproject.RegisterScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 //File Description: Creates activity page for the home screen page
 //Design/interface of this activity - stored in: activity_homescreenscreen.xml
-public class HomeScreen extends AppCompatActivity {
-    //private Button button;
-
+public class HomeScreen extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-        Button button = findViewById(R.id.button);
-        Button button2 = findViewById((R.id.button2));
-        button.setOnClickListener(new View.OnClickListener() {
+
+        Button button1 = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openActivity2();
+            public void onClick(View view) {
+                Intent int1 = new Intent(HomeScreen.this, LoginScreen.class);
+                startActivity(int1);
             }
         });
-
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openActivity3();
+            public void onClick(View view) {
+                Intent int2 = new Intent(HomeScreen.this, RegisterScreen.class);
+                startActivity(int2);
             }
         });
 
 
     }
 
-
-    public void openActivity2() {
-        Intent intent = new Intent(this, LoginScreen.class);
-        startActivity(intent);
-    }
-
-    public void openActivity3() {
-        Intent intent = new Intent(this, ScreenRegister.class);
-        startActivity(intent);
-    }
 }
+
---
+++ app/src/main/java/com/example/softwareengrproject/RegisterScreen.java
@@ -0,0 +1,35 @@
+package com.example.softwareengrproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class RegisterScreen extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_register);
+
+        Button button3 = (Button) findViewById(R.id.buttonStudent);
+        Button button4 = (Button) findViewById((R.id.buttonProf));
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent int3 = new Intent(RegisterScreen.this, LoginScreen.class);
+//                startActivity(int3);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent int4 = new Intent(RegisterScreen.this, LoginScreen.class);
+//                startActivity(int4);
+            }
+        });
+    }
+}
