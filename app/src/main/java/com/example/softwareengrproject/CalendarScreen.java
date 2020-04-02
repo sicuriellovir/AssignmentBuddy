a73e082662200db5d5ca0b74c1b4fcb39d56462f
Analise <analisesaunders@gmail.com>
2020-04-02 10:33:13 -0400

    Created Calendar and a supporting database to allow for events to be added

--- app/src/main/java/com/example/softwareengrproject/CalendarScreen.java
+++ app/src/main/java/com/example/softwareengrproject/CalendarScreen.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 public class CalendarScreen extends AppCompatActivity {
 
+    CustomCalendarView customCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        customCalendarView = (CustomCalendarView)findViewById(R.id.custom_calendar_view);
     }
 }
