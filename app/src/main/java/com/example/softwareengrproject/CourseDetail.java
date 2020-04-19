920cd9b0bb215b087fd02fcc54c9a0cc4c26e0f4
jeffreyknappman <jkk17b>
2020-04-19 14:48:08 -0400

    Added GradeDetail and GradeArray Adapter for having a scrollable list of grades for each class. Added some layout files for that as well.

--- app/src/main/java/com/example/softwareengrproject/CourseDetail.java
+++ app/src/main/java/com/example/softwareengrproject/CourseDetail.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
-
+//course detail displays the grades in a specific class in a listview
 public class CourseDetail extends AppCompatActivity
 {
-
     TextView text;
+    ListView gradeList;
+    String[] gradeNames = {"HW 1", "HW 2", "Test 1", "HW 3", "Quiz 1"};
+    String[] gradeDates = {"1/07/20", "1/24/20", "2/04/20", "2/24/20", "3/01/20"};
+    String[] grades = {"95", "93", "91", "86", "50"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,19 @@ public class CourseDetail extends AppCompatActivity
         setContentView(R.layout.activity_course_detail);
 
         text = (TextView) findViewById(R.id.textCourseDetail);
+        gradeList = (ListView) findViewById(R.id.gradeslist);
+        GradesArrayAdapter gradeAdapter = new GradesArrayAdapter(CourseDetail.this, gradeNames, gradeDates, grades);
+        gradeList.setAdapter(gradeAdapter);
+        gradeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(CourseDetail.this, GradeDetail.class);
+                intent.putExtra("gradeName", gradeNames[i]);
+                startActivity(intent);
+            }
+        });
 
-        Bundle bundle = getIntent().getExtras();
+        Bundle bundle = getIntent().getExtras();    //getting extra from Courses Activity
         if(bundle != null)
         {
             text.setText(bundle.getString("className"));
