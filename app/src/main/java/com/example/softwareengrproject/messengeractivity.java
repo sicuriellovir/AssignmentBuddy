c88808acc0c8329c7711af1bfc5af117f06fec15
ake1998 <ellis.ashley1998@gmail.com>
2020-04-21 10:34:55 -0400

    stuff for messenger

---
+++ app/src/main/java/com/example/softwareengrproject/messengeractivity.java
@@ -0,0 +1,14 @@
+package com.example.softwareengrproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class messengeractivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messengeractivity);
+    }
+}
