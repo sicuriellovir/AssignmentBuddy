b4e09a6997a5003f89bb8c50c64b16d97d5811e8
ake1998 <ellis.ashley1998@gmail.com>
2020-04-22 19:39:39 -0400

    new database file

--- app/src/main/java/com/example/softwareengrproject/messengeractivity.java
+++ app/src/main/java/com/example/softwareengrproject/messengeractivity.java
@@ -3,12 +3,26 @@ package com.example.softwareengrproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class messengeractivity extends AppCompatActivity {
 
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messengeractivity);
+
+        editText = (EditText) findViewById(R.id.messengerEditText);
+    }
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+
+        // Send in the chat
+        if (message.length() > 0) {
+            editText.getText().clear();
+        }
     }
 }
